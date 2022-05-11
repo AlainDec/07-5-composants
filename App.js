fd83d6c5b62afac8c8e04756ca86fe4626ee2067
unknown <jtwest@free.fr>
2022-05-11 12:30:34 +0200

    Create project

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, TouchableOpacity, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.touchable}>
+        <Text style={styles.text}>Félicitations</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +13,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center',    
   },
+  touchable: {
+    backgroundColor: 'green',
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '60%',
+    height: 50,
+    elevation: 8,
+  },
+  text: {
+    flex: 1,
+    textTransform: 'uppercase',
+    color: 'white',
+    alignItems: 'center',
+    alignSelf: 'center',
+    height: 50,
+  }
 });
